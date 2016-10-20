f2f6c0f2cfd200fb647718bff2113c9f8ad45446
mephi42 <mephi42@gmail.com>
2016-10-21 00:09:24 +0200

    Fix field offset

--- core-navigator.py
+++ core-navigator.py
@@ -137,11 +137,11 @@ def navigate(fp, ptr):
             while i < ptr_count:
                 needle = haystack[i]
                 if needle in vtables:
-                    print ('  @0x%.16x %s=0x%.16x offset=%d' %
-                           (addr - ptr_count * 8 + i * 8,
+                    print ('  @0x%.16x %s=0x%.16x field offset=%d' %
+                           (addr - (ptr_count - i) * 8,
                             vtables[needle],
                             needle,
-                            i * 8))
+                            (ptr_count - i) * 8))
                 i += 1
 
             pos += 1
